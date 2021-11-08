900ea55cf94ba6d200b95f11229d67dfedf3c373
sam <samzuhal095@gmail.com>
2021-11-08 21:18:37 +0800

    latihan webrtc pada console

---
+++ WebRTC/WebRTC API/latihan1.js
@@ -0,0 +1,22 @@
+const peer = new RTCPeerConnection();
+const dataChannel = peer.createDataChannel("channel");
+dataChannel.onmessage = ev => console.log("Pesan Baru: ",ev.data);
+dataChannel.onopen = () => console.log("Peer Connected!!!");
+peer.onicecandidate = () => console.log(JSON.stringify(peer.localDescription));
+peer.createOffer().then(offer => peer.setLocalDescription(offer));
+
+
+///////////////////////////////////////////////////
+const peerB = new RTCPeerConnection();
+const peerB.onicecandidate = () => console.log(JSON.stringify(peerB.localDescription));
+let dataChannelB;
+peerB.ondatachannel = e => {
+	dataChannelB = e.channel;
+	dataChannelB.onmessage = e => console.log("Pesan Baru: ", e.data);
+	dataChannelB.onopen = () => console.log("Peer Connected!!!");
+}
+peerB.setRemoteDescription(offer);
+peerB.createAnswer().then(answer => peerB.setLocalDescription(answer));
+
+////////////////////////////////////////////////////////
+dataChannel.send("Hello World");
