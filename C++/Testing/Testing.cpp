ca0b1eb49d0e0d549d3898ceadd861b2be651c19
samzuhalsetiawan <samzuhalsetiawan@gmail.com>
2021-12-05 02:04:32 +0800

    main sebentar dengan c++

--- C++/Testing/Testing.cpp
+++ C++/Testing/Testing.cpp
@@ -1,5 +1,11 @@
 #include<iostream>
 
 void main() {
-	std::cout << "Hello World" << std::endl;
+
+	int arrayKu[10]{};
+
+	for (int a = 0; a < 10; a++) {
+		arrayKu[a] = a;
+	}
+	std::cout << arrayKu[5] << std::endl;
 }
