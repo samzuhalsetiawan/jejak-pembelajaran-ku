b32bd8ac2d5ca6278d700d09ddc832a84146cc31
samzuhalsetiawan <samzuhalsetiawan@gmail.com>
2022-01-17 08:20:25 +0800

    kembali memperdalam pemahaman recycler view

--- Android/AndroidStudioProjects/LatihanRecyclerView/app/src/main/java/com/example/latihanrecyclerview/ItemAdapter.kt
+++ Android/AndroidStudioProjects/LatihanRecyclerView/app/src/main/java/com/example/latihanrecyclerview/ItemAdapter.kt
@@ -1,4 +1,30 @@
 package com.example.latihanrecyclerview
 
-class ItemAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemAdapter(val items: List<String>): RecyclerView.Adapter<ItemAdapter.ItemHolder>() {
+
+    class ItemHolder(val view: View): RecyclerView.ViewHolder(view) {
+        val textView = view.findViewById<TextView>(R.id.tv_item_list)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.item_list, parent, false)
+        return ItemHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ItemHolder, position: Int) {
+        holder.textView.text = items[position]
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
 }
--- Android/AndroidStudioProjects/LatihanRecyclerView/app/src/main/java/com/example/latihanrecyclerview/MainActivity.kt
+++ Android/AndroidStudioProjects/LatihanRecyclerView/app/src/main/java/com/example/latihanrecyclerview/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.latihanrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.rv_activity_main)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = ItemAdapter(createDummyList())
+
     }
+
+    fun createDummyList(): List<String> {
+        return List(100){ "Item ke $it" }
+    }
+
 }
