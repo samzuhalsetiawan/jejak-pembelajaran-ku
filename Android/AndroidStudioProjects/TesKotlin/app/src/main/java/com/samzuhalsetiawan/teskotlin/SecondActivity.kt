c8d35aa9396d83e7e03117b3ca1eca299f3f713d
samzuhalsetiawan <samzuhalsetiawan@gmail.com>
2022-03-14 23:43:44 +0800

    membuat project khusus untuk pengetesan kode kotlin

--- Android/AndroidStudioProjects/TesKotlin/app/src/main/java/com/samzuhalsetiawan/teskotlin/SecondActivity.kt
+++ Android/AndroidStudioProjects/TesKotlin/app/src/main/java/com/samzuhalsetiawan/teskotlin/SecondActivity.kt
@@ -1,11 +1,25 @@
 package com.samzuhalsetiawan.teskotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 
 class SecondActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        findViewById<Button>(R.id.btnSendResult).setOnClickListener {
+            val intent = Intent().apply {
+                putExtra("EXTRA_DATA", "Hello Dunia")
+            }
+            setResult(RESULT_OK, intent)
+            finish()
+        }
+
     }
+
 }
