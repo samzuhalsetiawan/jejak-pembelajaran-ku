bea66a04b2189eafa60dca6eaaddafefd89cf921
samzuhalsetiawan <samzuhalsetiawan@gmail.com>
2022-04-18 00:10:23 +0800

    extracting

--- Python/AudioExtractor/audioExtractor.py
+++ Python/AudioExtractor/audioExtractor.py
@@ -1,5 +1,5 @@
 import moviepy.editor as mp
 
-my_clip = mp.VideoFileClip(r"videoplayback.mp4")
+my_clip = mp.VideoFileClip(r"video.mp4")
 
-my_clip.audio.write_audiofile(r"ayat_kursi.mp3")
+my_clip.audio.write_audiofile(r"audio.mp3")
