76457068226a1477603f6f85f5af1b8bc247dd9a
samzuhalsetiawan <samzuhalsetiawan@gmail.com>
2022-06-30 19:23:28 +0800

    Memindahkan project ke folder freecodecamp

---
+++ belajar_webpack/freecodecamp/webpack.common.js
@@ -0,0 +1,19 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: {
+        main: "./src/index.js",
+        vendor: "./src/vendor.js"
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: "./src/template.html"
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: ["html-loader"]
+            }
+        ]
+    }
+};
